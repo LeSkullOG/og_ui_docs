be71b9ad82523793f9e0251abd27de61f6728c9b
LeSkullOG <284369431+LeSkullOG@users.noreply.github.com>
2026-05-13 21:01:49 +0100

    Document chat command suggestion ACE option

--- assets/chat.md.DV4lbKCr.lean.js
+++ assets/chat.md.DV4lbKCr.lean.js
@@ -3,7 +3,7 @@ const __pageData = JSON.parse('{"title":"Chat Skin","description":"","frontmatte
 const _sfc_main = { name: "chat.md" };
 function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
   return openBlock(), createElementBlock("div", null, [..._cache[0] || (_cache[0] = [
-    createStaticVNode("", 20)
+    createStaticVNode("", 21)
   ])]);
 }
 const chat = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
