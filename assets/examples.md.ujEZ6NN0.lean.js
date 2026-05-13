319dbd66b952a2d907d045254530c0c1bde17387
LeSkullOG <284369431+LeSkullOG@users.noreply.github.com>
2026-05-13 17:27:32 +0100

    Update docs examples for showcase command

--- assets/examples.md.ujEZ6NN0.lean.js
+++ assets/examples.md.ujEZ6NN0.lean.js
@@ -3,7 +3,7 @@ const __pageData = JSON.parse('{"title":"Examples","description":"","frontmatter
 const _sfc_main = { name: "examples.md" };
 function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
   return openBlock(), createElementBlock("div", null, [..._cache[0] || (_cache[0] = [
-    createStaticVNode("", 11)
+    createStaticVNode("", 12)
   ])]);
 }
 const examples = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
