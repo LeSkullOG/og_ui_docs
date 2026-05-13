aa2700e4b1940390f7d640519389f261c437152d
LeSkullOG <284369431+LeSkullOG@users.noreply.github.com>
2026-05-13 18:26:37 +0100

    Document notification action buttons

--- assets/menus.md.7LhF50bC.lean.js
+++ assets/menus.md.7LhF50bC.lean.js
@@ -3,7 +3,7 @@ const __pageData = JSON.parse('{"title":"Menus & Dialogs","description":"","fron
 const _sfc_main = { name: "menus.md" };
 function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
   return openBlock(), createElementBlock("div", null, [..._cache[0] || (_cache[0] = [
-    createStaticVNode("", 23)
+    createStaticVNode("", 27)
   ])]);
 }
 const menus = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
