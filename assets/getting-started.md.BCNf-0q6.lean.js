22320594d065ff4ffdbed45c7aca7412c74cc4d7
LeSkullOG <284369431+LeSkullOG@users.noreply.github.com>
2026-05-13 21:42:10 +0100

    Clean public developer docs

--- assets/getting-started.md.BCNf-0q6.lean.js
+++ assets/getting-started.md.BCNf-0q6.lean.js
@@ -1,9 +1,9 @@
 import { _ as _export_sfc, o as openBlock, c as createElementBlock, ag as createStaticVNode } from "./chunks/framework.BFOyFJ_i.js";
-const __pageData = JSON.parse('{"title":"Install & Build","description":"","frontmatter":{},"headers":[],"relativePath":"getting-started.md","filePath":"getting-started.md"}');
+const __pageData = JSON.parse('{"title":"Installation","description":"","frontmatter":{},"headers":[],"relativePath":"getting-started.md","filePath":"getting-started.md"}');
 const _sfc_main = { name: "getting-started.md" };
 function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
   return openBlock(), createElementBlock("div", null, [..._cache[0] || (_cache[0] = [
-    createStaticVNode("", 18)
+    createStaticVNode("", 14)
   ])]);
 }
 const gettingStarted = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
--- assets/grid.md.LwJ9oF9Q.lean.js
+++ assets/grid.md.LwJ9oF9Q.lean.js
@@ -1,13 +1,13 @@
 import { _ as _export_sfc, o as openBlock, c as createElementBlock, ag as createStaticVNode } from "./chunks/framework.BFOyFJ_i.js";
-const __pageData = JSON.parse('{"title":"Chat Skin","description":"","frontmatter":{},"headers":[],"relativePath":"chat.md","filePath":"chat.md"}');
-const _sfc_main = { name: "chat.md" };
+const __pageData = JSON.parse('{"title":"Grid UI","description":"","frontmatter":{},"headers":[],"relativePath":"grid.md","filePath":"grid.md"}');
+const _sfc_main = { name: "grid.md" };
 function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
   return openBlock(), createElementBlock("div", null, [..._cache[0] || (_cache[0] = [
-    createStaticVNode("", 31)
+    createStaticVNode("", 10)
   ])]);
 }
-const chat = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+const grid = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
 export {
   __pageData,
-  chat as default
+  grid as default
 };
