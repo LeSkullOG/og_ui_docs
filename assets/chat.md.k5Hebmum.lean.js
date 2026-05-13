5a52a21e6b613237518a5a00c6dd77a7e83aa94a
LeSkullOG <284369431+LeSkullOG@users.noreply.github.com>
2026-05-13 20:18:13 +0100

    Add chat skin documentation

---
+++ assets/chat.md.k5Hebmum.lean.js
@@ -0,0 +1,13 @@
+import { _ as _export_sfc, o as openBlock, c as createElementBlock, ag as createStaticVNode } from "./chunks/framework.BFOyFJ_i.js";
+const __pageData = JSON.parse('{"title":"Chat Skin","description":"","frontmatter":{},"headers":[],"relativePath":"chat.md","filePath":"chat.md"}');
+const _sfc_main = { name: "chat.md" };
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, [..._cache[0] || (_cache[0] = [
+    createStaticVNode("", 13)
+  ])]);
+}
+const chat = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export {
+  __pageData,
+  chat as default
+};
--- assets/exports.md.fJKdsbn5.lean.js
+++ assets/exports.md.fJKdsbn5.lean.js
@@ -3,7 +3,7 @@ const __pageData = JSON.parse('{"title":"Exports","description":"","frontmatter"
 const _sfc_main = { name: "exports.md" };
 function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
   return openBlock(), createElementBlock("div", null, [..._cache[0] || (_cache[0] = [
-    createStaticVNode("", 13)
+    createStaticVNode("", 15)
   ])]);
 }
 const exports$1 = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
